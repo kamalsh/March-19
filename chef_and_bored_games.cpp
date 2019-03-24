e1ac99abca5abddc48cf993add4908a8cc486f08
Kamal Sharma <36453230+kamalsh@users.noreply.github.com>
2019-03-24 21:56:28 +0530

    Update chef_and_bored_games.cpp

--- chef_and_bored_games.cpp
+++ chef_and_bored_games.cpp
@@ -2,7 +2,13 @@
 
 Since putting in long hours is not an easy task, Chef's mind wanders elsewhere. He starts counting the number of squares with odd side length on his chessboard..
 
-However, Chef is not satisfied. He wants to know the number of squares of odd side length on a generic N∗N chessboard.  */
+However, Chef is not satisfied. He wants to know the number of squares of odd side length on a generic N∗N chessboard.
+
+Input:
+The first line will contain a single integer T, the number of test cases.
+The next T lines will have a single integer N, the size of the chess board.
+Output:
+For each test case, print a integer denoting the number of squares with odd length.*/
 
 #include<bits/stdc++.h>
 using namespace std;
